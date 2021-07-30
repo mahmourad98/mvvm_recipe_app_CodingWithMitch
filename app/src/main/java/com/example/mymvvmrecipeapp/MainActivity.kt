6bb741c14498bf5071d97fc55cba9fa61f33f2c6
mahmoudmourad1998 <44194590+mahmoudmourad1998@users.noreply.github.com>
2021-07-30 03:09:10 +0300

    intialized02

--- app/src/main/java/com/example/mymvvmrecipeapp/MainActivity.kt
+++ app/src/main/java/com/example/mymvvmrecipeapp/MainActivity.kt
@@ -3,11 +3,20 @@ package com.example.mymvvmrecipeapp
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material.Button
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.alpha
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.mymvvmrecipeapp.ui.theme.MyMVVMRecipeAppTheme
 
 class MainActivity : ComponentActivity() {
@@ -15,7 +24,15 @@ class MainActivity : ComponentActivity() {
     super.onCreate(savedInstanceState)
     setContent {
       MyMVVMRecipeAppTheme {
-
+        Column(Modifier.padding(16.dp)){
+          Text(text = "Hi")
+          Spacer(modifier = Modifier.padding(top = 10.dp, bottom = 10.dp))
+          Text(text = "First Android App with JetPack Compose")
+          Spacer(modifier = Modifier.padding(top = 10.dp, bottom = 10.dp))
+          Button(onClick = {}, Modifier.size(128.dp)) {
+            Text(text = "Button")
+          }
+        }
       }
     }
   }
